a5418407a91ada8ce2e7b036dea865341e6e689a
Anthony Guzzardo <aguzzar2@depaul.edu>
2024-06-11 14:21:53 -0500

    can only select upcoming games

--- app/addisonliquors-parking-app/routes/index.js
+++ app/addisonliquors-parking-app/routes/index.js
@@ -6,14 +6,11 @@ const User = require('../models/user')
 
 router.get('/', async (req, res) => {
     try {
-        const reservations = await Reservation.find({}).populate('user').exec()
-        res.render('index', {
-            reservations: reservations
-        })
-    } catch (err) {
-        console.error('Error fetching reservations:', err)
+        const reservations = await Reservation.find({}).populate('user').populate('game').exec()
+        res.render('index', { reservations: reservations })
+    } catch (error) {
+        console.error('Error fetching reservations:', error)
         res.redirect('/')
     }
 })
-
 module.exports = router
